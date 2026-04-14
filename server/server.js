6f84688bcf2c14028082019c75a37f68d5ef1af2
Sapna615 <142426825+Sapna615@users.noreply.github.com>
2026-04-14 17:37:48 +0530

    Fix deployment error: Correct auth routes import path

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
-const authRouter = require("./routes/api/auth/auth-routes");
+const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 const adminOrderRouter = require("./routes/admin/order-routes");
 const adminDashboardRouter = require("./routes/admin/dashboard-routes");
